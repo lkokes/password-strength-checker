922541057d3840f32819274da3ef2909b5645d20
Lucia Kokesova <lkokesova@gmail.com>
2021-10-04 23:31:09 +0200

    Add strength bar and set bar color based on strength

--- script.js
+++ script.js
@@ -18,6 +18,36 @@ function strengthChecker(){
     let barLength = Object.values(parameters).filter(value => value);
 
     console.log(Object.values(parameters), barLength);
+
+    strengthBar.innerHTML = "";
+    for(let i in barLength){
+        let span = document.createElement("span");
+        span.classList.add("strength");
+        strengthBar.appendChild(span);
+    }
+
+    let spanRef = document.getElementsByClassName("strength");
+    
+    for(let i=0; i < spanRef.length; i++){
+        switch(spanRef.length -1){
+            case 0:
+                spanRef[i].style.background = "#ff3e36";
+                msg.textContent = "Your password is very weak";
+                break;
+            case 1:
+                spanRef[i].style.background = "#ff691f";
+                msg.textContent = "Your password is weak";
+                break;
+            case 2:
+                spanRef[i].style.background = "#ffda36";
+                msg.textContent = "Your password is good";
+                break;
+            case 3:
+                spanRef[i].style.background = "#0be881";
+                msg.textContent = "Your password is strong";
+                break;
+        }
+    }
 }
 
 function toggle(){
